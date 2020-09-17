c04d3ef3c507f22ce24b732824ba05db02bb3969
Morgan Little <mrgnlit@gmail.com>
2020-09-16 17:24:42 -0700

    fixes to js

--- app.js
+++ app.js
@@ -5,31 +5,30 @@
 //     url: 
 // };
 
-function getDogImage() {
-    dogNumber = $('input[type=number][name=quantity]').val('')
-  fetch(`https://dog.ceo/api/breeds/image/random/${dogNumber}`)
+function getDogImage(numberOfDogs) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${numberOfDogs}`)
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
-    console.log(dogNumber);
+    console.log(numberOfDogs + " dogs");
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
-  //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
-  )
-  //display the results section
-  $('.results').removeClass('hidden');
+   //replace the existing image with the new one
+//   $('.results-img').replaceWith(
+//     `<img src="${responseJson.message}" class="results-img">`
+//   )
+   //display the results section
+//   $('.results').removeClass('hidden');
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    // let dogNumber = $('input[type=number][name=quantity]').val('');
-    getDogImage();
+    let numberOfDogs = $('input[type="number"]').val()
+    getDogImage(numberOfDogs);
   });
 }
 
